93fbb15ad07d2649f0f253cc685d98babf6b5598
RanggaWbp <ranggawbp16@gmail.com>
2026-09-05 15:54:52 +0700

    ReSukiSU: add missing ksu_handle_newfstat_ret/fstat64_ret hooks in fs/stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -127,6 +127,10 @@ EXPORT_SYMBOL(vfs_fstat);
 extern struct static_key_true ksu_su_compat_enabled;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
+#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
+extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);
+#endif
 #endif
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
@@ -361,6 +365,7 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
 
+	ksu_handle_newfstat_ret(&fd, &statbuf);
 	return error;
 }
 
@@ -479,6 +484,7 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
 
+	ksu_handle_fstat64_ret(&fd, &statbuf);
 	return error;
 }
 
