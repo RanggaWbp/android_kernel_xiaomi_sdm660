faa7c21a6fc6dfa275cc1ba264f8582865b3d701
RanggaWbp <ranggawbp16@gmail.com>
2026-09-12 16:20:46 +0700

    fix(exec.c): include susfs_def.h + remove leftover CONFIG_KSU_MANUAL_HOOK guard

--- fs/exec.c
+++ fs/exec.c
@@ -1706,12 +1706,6 @@ out_ret:
 	return retval;
 }
 
-#if defined(CONFIG_KSU_MANUAL_HOOK) || defined(CONFIG_KSU_SUSFS)
-__attribute__((hot))
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
-				void *argv, void *envp, int *flags);
-#endif
-
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
